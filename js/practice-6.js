286fc6df81ed1698baa7affc0b21bcd2ecbef979
Sergey-Proskurnin <proskurnin@ua.fm>
2021-02-13 22:44:35 +0200

    Добавляет новые решения из практики

---
+++ js/practice-6.js
@@ -0,0 +1,25 @@
+class Box {
+    constructor def {
+
+    } 
+
+}
+
+const div = new Box({
+    selector: '.div',
+    url: '',
+    description: 'Какой-то текст',
+    size: 300,
+    title: "Some Title"
+})
+
+const div = new Box({
+    selector: '.div',
+    url: '',
+    description: 'Какой-то текст',
+})
+const div = new Box({
+    selector: '.div',
+    size: 200,
+    color: 'red',
+})
--- js/task-3r.js
+++ js/task-3r.js
@@ -1,11 +1,22 @@
 import users from './users.js';
 
-// Получить массив имен пользователей по полу (поле gender).
+// // Получить массив имен пользователей по полу (поле gender).
 
+// const getUsersWithGender = (users, gender) => {
+// return users.filter(user => user.gender === gender).map(({name}) => name)
+//   // твой код
+// };
+//** */ решение с редюсом **//
 const getUsersWithGender = (users, gender) => {
-return users.filter(user => user.gender === gender).map(({name}) => name)
-  // твой код
+  return users.reduce((acc, user) => {
+    if (user.gender === gender) {
+      acc.push(user.name);
+    }
+    return acc;
+  }, []);
 };
 
-console.log(getUsersWithGender(users, 'male')); 
+
+
+console.log(getUsersWithGender(users, 'male'));
 // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
